32676217096cdaa9c323d8782436bb0261d5c7ec
cuongdn125 <ngoccuong061@gmail.com>
2022-03-23 16:04:13 +0700

    update

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,6 +1,9 @@
 function parseCookies(token) {
   var list = {};
   var rc = token;
+  if (!token) {
+    throw new Error("Cookie not found");
+  }
   rc.split(";").forEach(function (cookie) {
     var parts = cookie.split("=");
     list[parts.shift().trim()] = decodeURI(parts.join("="));
